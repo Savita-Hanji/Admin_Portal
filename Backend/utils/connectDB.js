a02cb993f0cd45e982933da99ad81c62e8f982dd
Atharva Hanchate <adityadhanshett@gmail.com>
2025-12-31 00:50:41 +0530

    fix: correct indentation in MongoDB connection options

--- Backend/utils/connectDB.js
+++ Backend/utils/connectDB.js
@@ -5,8 +5,8 @@ config();
 const connectDB = async () => {
   try {
     const con = await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
     });
 
     console.log(`✅ MongoDB Connected: ${con.connection.host}`);
